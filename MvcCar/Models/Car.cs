27656e20c3057e94505b29d0253d41c1c7caf93d
RemusLuht <remusluht2005@gmail.com>
2022-12-04 00:33:07 +0200

    Add validation rules

--- MvcCar/Models/Car.cs
+++ MvcCar/Models/Car.cs
@@ -7,9 +7,17 @@ namespace MvcCar.Models
     public class Car
     {
         public int Id { get; set; }
+        [StringLength(20, MinimumLength = 1)]
+        [Required]
         public string Name { get; set; }
+        [StringLength(20, MinimumLength = 3)]
+        [Required]
         public string Color { get; set; }
+        [Range(1,300)]
+        [Required]
         public int Speed { get; set; }
+        [StringLength(10, MinimumLength = 5)]
+        [Required]
         public string Transmission { get; set; }
     }
 }
