10ecc331036950993be5f0cf3b4dc91ced2eb572
RemusLuht <remusluht2005@gmail.com>
2022-12-03 23:17:37 +0200

    Scaffold movie pages

---
+++ MvcCar/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+    }
+}
